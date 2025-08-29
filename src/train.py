64faa94e49d41aef94fd3da6fe7e6168d49814b7
poojaparameswaran99 <poojaparameswaran99@github.com>
2025-08-29 18:34:59 -0400

    train update

--- src/train.py
+++ src/train.py
@@ -2,24 +2,29 @@ import pandas as pd
 import numpy as np 
 import torch
 from functools import partial
+import math
 import re
 from torch.utils.data import Dataset, DataLoader
 import h5py
 import ast
 from pathlib import Path
+from datetime import datetime
 import json
 import os 
 import torch.nn as nn
 import hydra 
-import sys 
-from model import Model
-from loss import MyTripletLoss
-from metrics import compute
-import wandb
+import random
+import sys
+sys.path.append(os.path.expanduser('~/soderlinglab/user/pooja/projects/Biotin/src'))
 from dataset import LysineDataset
+from metrics import compute  # whatever you need
+from collections import Counter
+import wandb
 import hydra
 from omegaconf import DictConfig, OmegaConf
-sys.path.append(os.path.expanduser('~/soderlinglab/utils'))
+from loss import MarginScheduledTripletLossFunction
+from hydra.utils import get_original_cwd
+sys.path.append(os.path.expanduser('~/soderlinglab/user/pooja/projects/Biotin/src')) ## str(Path(get_original_cwd()))
 
 # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3)
 
@@ -39,131 +44,156 @@ sys.path.append(os.path.expanduser('~/soderlinglab/utils'))
 
 #### actionables
 ## update lr and margin according to an annealing scheme.
-@hydra.main(version_base=None, config_path="configs", config_name="train")
+@hydra.main(version_base=None, config_path="configs", config_name="trainn")
 def main(cfg: DictConfig): # 
     cfg = OmegaConf.to_container(cfg, resolve = True)
-    
+    train_model(cfg)
     return
 
-def validate(df, model, TMLcriterion, BCEcriterion, lmbd1, lmbd2, embeddings_path):
-    dataset = LysineDataset(df, 'Accession','BiotinPosition','seq', embeddings_path)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+def validate(cfg, val_loader, model, Loss1, Loss2, alpha):
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print('validation Device ', DEVICE)
-    loss = 0
+    l = 0
     model.eval()
+    total_labels = []
+    total_preds = []
     with torch.no_grad():
-        for batch in dataloader:
+        for batch in val_loader:
             embed, labels, pos_len = batch
             embed = embed.to(DEVICE)
             labels = labels.to(DEVICE)
             pos_len = pos_len.to(DEVICE)
             embed, preds = model(embed)
             embed = embed.squeeze(0)
-            labels = labels.flatten().cpu().clone()
-            preds = preds.flatten().cpu().clone()
-            acc, auroc, aupr, CM = compute(labels, preds, threshold=0.5)
-            loss1 = TMLcriterion(embed, pos_len)
-            loss2 = BCEcriterion(preds, labels) ## params
+            preds = preds.squeeze(-1)
+            total_labels.append(labels.flatten().cpu().clone())
+            total_preds.append(preds.flatten().cpu().clone())
+            loss1 = Loss1(embed, pos_len)
+            loss2 = Loss2(preds, labels) ## params
             # Backward pass
-            loss += ((lmbd1*loss1) + (lmbd2*loss2)).item()
-    loss = loss/len(dataloader)
+            l += (alpha*loss1 + (1-alpha)*loss2).item()
+#             l += loss2
+    loss = l/len(val_loader)
+    total_labels = torch.cat(total_labels)
+    total_preds = torch.cat(total_preds)
+    acc, auroc, aupr, CM = compute(total_labels, total_preds, threshold=0.5)
     return acc, auroc, aupr, CM, loss
 
 def load_data(cfg, cross='train'):
     ## implement cross val
     dataset_builder = hydra.utils.instantiate(cfg['data']['dataset'], _partial_=True)
-    dataset = dataset_builder(file=cfg['data']['dataset'][cross]['file'])
-    dataloader_builder = hydra.utils.instantiate(cfg.dataloader, _partial_=True)
+    dataset = dataset_builder(file=cfg['data']['splits'][cross]['file'])
+    dataloader_builder = hydra.utils.instantiate(cfg['data']['dataloader'], _partial_=True)
     dataloader = dataloader_builder(dataset)
     return dataloader
 
-def train_model(lmbd1=0.5, lmbd2=0.5, alternate_epochs=False):
-    # Setup data
-    embeddings_path = '/cwork/pkp14/Biotin/esm2_15b_embeds'  # your embeddings path
-    train_csv = pd.read_csv(f'~/soderlinglab/user/pooja/projects/Biotin/data/train-val-test/train.csv')
-    train_df = train_csv.drop(columns=list(filter(lambda x: x.startswith('Unnamed'), train_csv.columns)))
-    val_file = f'~/soderlinglab/user/pooja/projects/Biotin/data/train-val-test/val.csv'
-    val_df = pd.read_csv(val_file)
-    val_df = val_df.drop(columns=list(filter(lambda x: x.startswith('Unnamed'), val_df.columns)))
-    # Create dataset and dataloader
-    dataset = LysineDataset(train_df, 'Accession','BiotinPosition','seq', embeddings_path)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)  # batch_size=1 for now
-    print('train data', train_df)
-    print('validation data', val_df)
+def train_model(cfg):
+    ## data
+    train_loader = load_data(cfg, 'train')
+    val_loader = load_data(cfg, cross='val')
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print('training in DEVICE', DEVICE)
+    print('training on DEVICE', DEVICE)
+    
+    ## instantiate
+    Loss1 = hydra.utils.instantiate(cfg['Loss1'])
+    Loss2 = hydra.utils.instantiate(cfg['Loss2'])
+    model = hydra.utils.instantiate(cfg['models']['esm2_15b'])
+    BURN_IN = cfg['burn_in_epochs']
     # Training parameters
-    num_epochs = 100
-    learning_rate = 0.001
-    if not alternate_epochs:
-        l1n = str(lmbd1).replace('.', '')
-        l2n = str(lmbd2).replace('.', '')
-        NAME=f'v1_lmbd{l1n}lmbd{l2n}'
-    else:
-        NAME=f'v1_alternateEpochs'
+    alternate_epochs = bool(cfg.get('alternate_epochs', False))
+    num_epochs = cfg['n_epochs']
+    learning_rate = cfg['lr']
+    job = cfg['job_type']
+    
+    # Nice run name + group
+    l1n = str(cfg['loss1_weight']).replace('.', '')
+    l2n = str(1 - cfg['loss1_weight']).replace('.', '')
+    m   = str(cfg['anneal_fn']['M_0']).replace('.', '')
+    time = datetime.now().strftime("%m_%d-%H_%M")
+    nae = cfg['n_alternate_epoch']
+    NAME = f"alternatEpoch{nae}_{m}" if alternate_epochs else f"not"
+    group = cfg["wandb"]["group"]   # <- this will be "euclidean"
+    proj = cfg['project']
+    seed = cfg['seed']
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # start
     min_loss =float('inf')
     run = wandb.init(
-        # Set the wandb entity where your project will be logged (generally your team name).
         entity="soderlinglab-dukecellbio",
-        # Set the wandb project where this run will be logged.
-        project="biotin2",
+        project=proj,
+        job_type= job,
         name=NAME,
-        # Track hyperparameters and run metadata.
-        config={
-                "learning_rate": 0.02,
-                "architecture": "MLP",
-                "dataset": "train",
-                "epochs": num_epochs,
-            },
+        group=str(group),
+        config=cfg,
         )
-    # Loss function
-    TMLcriterion = MyTripletLoss()
-    BCEcriterion = nn.BCELoss()
-    model = Model()
     model.to(DEVICE)
+    ## make hydra compatible
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    model.train()
     for epoch in range(num_epochs):
+        model.train()
         epoch_loss = 0.0
-
-        for batch_idx, batch in enumerate(dataloader):
+        if alternate_epochs:
+            alpha = 1 if (epoch %nae ==0) else 0 ## update bce every 5 epochs
+        else:
+            alpha = cfg['loss1_weight']
+        pnc = Counter()
+        for batch_idx, batch in enumerate(train_loader):
             embed, labels, pos_len= batch # seq_embedding, biotin_pos
+            pnc.update(labels.view(-1).tolist())
             embed = embed.to(DEVICE)
-            labels = labels.to(DEVICE)
+            labels = labels.to(DEVICE).float()
             pos_len = pos_len.to(DEVICE)
             # Create model for this sequence
             embed, preds = model(embed)
             embed = embed.squeeze(0)
             # Create optimizer for this model
             # Zero gradients
-            optimizer.zero_grad()
-            
+            optimizer.zero_grad(set_to_none=True)
+            preds = preds.squeeze(-1)
             # Calculate loss
-            loss1 = TMLcriterion(embed, pos_len)
-            loss2 = BCEcriterion(preds, labels) ## params
-            # Backward pass
-            l = ((lmbd1*loss1) + (lmbd2*loss2))
+            loss1 = Loss1(embed, pos_len)
+            loss2 =  Loss2(preds, labels) ## params
+            l = alpha*loss1 + (1-alpha)*loss2
+#             l = loss2
             l.backward()
+            ## optimizer step
             optimizer.step()
+            ## update margin
+            Loss1.step()
             ## dim as [b, nr, 1], FLATTEN TO nr only for metrics
             ## flatten for metrics
             labels = labels.flatten().detach().cpu().clone()
             preds = preds.flatten().detach().cpu().clone()
-        v_acc, v_auroc, v_aupr, v_CM, v_loss = validate(val_df, model, TMLcriterion, BCEcriterion,
-                                                       lmbd1, lmbd2, embeddings_path)
+        v_acc, v_auroc, v_aupr, v_CM, v_loss = validate(cfg, val_loader, model, Loss1, Loss2, alpha)
         val_metrics = {"acc": float(v_acc), "loss": float(v_loss), 'auroc': float(v_auroc), 'aupr': float(v_aupr)}
         run.log(val_metrics)
         print(f'Epoch [{epoch+1}/{num_epochs}]; Val Loss: {v_loss:.4f}')
-        if v_loss < min_loss and epoch > 10:
-            with open(f'/cwork/pkp14/Biotin/models/{NAME}.json','w') as f:
-                json.dump(val_metrics, f, indent=4)
+        if v_loss < min_loss and epoch > BURN_IN:
+            safe_pnc = float(pnc) if hasattr(pnc, "__float__") else pnc
+            val_metrics.update({'epoch' : epoch, 'pnc': safe_pnc})
+            extra_data = {'embedding': tuple(embed.shape), 'preds': tuple(preds.shape),
+                         'preds_min': preds.min().item(),
+                         'preds_max': preds.max().item(), 'margin': float(Loss1.margin)
+                         }
+            
+            art = wandb.Artifact(name=f"{NAME}-eval", type="eval", metadata=extra_data)
+            with art.new_file("val_metrics.json", mode="w") as f:
+                json.dump(val_metrics
+                          , f, indent=2)
+            with art.new_file("extra_data.json", mode="w") as f:
+                json.dump(extra_data, f, indent=2)
+            run.log_artifact(art, aliases=["best"])
             torch.save(model.state_dict(), f'/cwork/pkp14/Biotin/models/{NAME}.pth')
-            print(f"New best model saved with loss: {v_loss:.4f}")
+            print(f"New best model @ epoch {epoch} saved with loss: {v_loss:.4f}")
+            print(f'POS:NEG ratio {pnc}')
             min_loss = v_loss
     run.finish()
     return
 
 
 if __name__ == "__main__":
-    train_model(lmbd1=0.5, lmbd2=0.5)
+    main()
+
