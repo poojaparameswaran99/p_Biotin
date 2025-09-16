2692cda00d040e1c5aec7ef785d2c896e101750a
poojaparameswaran99 <poojaparameswaran99@github.com>
2025-09-16 10:42:20 -0400

    model6 w 4 layers  reduced relu count

--- src/model.py
+++ src/model.py
@@ -51,7 +51,6 @@ class Model6(nn.Module):
         return x0, x1
     
 
-## Model1 = mlp w scalar sigmoidal
 class Model1(nn.Module):
     def __init__(self, in_dim=5120,hidden_dim=2560, out_dim=1280):
         super(Model1, self).__init__()
@@ -73,83 +72,6 @@ class Model1(nn.Module):
         x = self.Sigmoid(x).squeeze(-1) # batch, rows, cols, [1, RES_NUM, 1]
         return TMLx, x # only removes dim =1, picks last
 
-## Model2 == mlp + sigmoid
-class Model2(nn.Module):
-    def __init__(self, in_dim=5120,hidden_dim=2560, out_dim=1280):
-        super(Model2, self).__init__()
-        self.indim = in_dim
-        self.hiddim = hidden_dim
-        self.outdim = out_dim
-        self.fc1 = nn.Linear(in_dim , hidden_dim)
-        nn.init.xavier_normal_(self.fc1.weight)
-        self.fc2 = nn.Linear(hidden_dim, out_dim)
-        nn.init.xavier_normal_(self.fc2.weight)
-        self.fc3 = nn.Linear(out_dim, 1)
-        nn.init.xavier_normal_(self.fc3.weight)
-        self.Sigmoid = nn.Sigmoid()
-        
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x)) ## out 1280
-        sigmoidal_proj = self.Sigmoid(x) ## out 1280
-        x = self.fc3(sigmoidal_proj)
-        x = self.Sigmoid(x)
-        return sigmoidal_proj, x 
-
-
-## Model2 == mlp + sigmoid
-class Model3(nn.Module):
-    def __init__(self, in_dim=5120,hidden_dim=2560, out_dim=1280):
-        super(Model3, self).__init__()
-        self.indim = in_dim
-        self.hiddim = hidden_dim
-        self.outdim = out_dim
-        self.fc1 = nn.Linear(in_dim , hidden_dim)
-        nn.init.xavier_normal_(self.fc1.weight)
-        self.fc2 = nn.Linear(hidden_dim, out_dim)
-        nn.init.xavier_normal_(self.fc2.weight)
-        self.fc3 = nn.Linear(out_dim, 1)
-        nn.init.xavier_normal_(self.fc3.weight)
-        self.Sigmoid = nn.Sigmoid()
-        
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x)) ## out 1280
-        x0 = self.fc3(x)
-        x1 = self.Sigmoid(x0)
-        return x0, x1
-
-class Model4(nn.Module):
-    def __init__(self, in_dim=5120,hidden_dim=2560, out_dim=1280):
-        super(Model4, self).__init__()
-        self.indim = in_dim
-        self.hiddim = hidden_dim
-        self.outdim = out_dim
-        
-        self.fc1 = nn.Linear(in_dim , hidden_dim)
-        self.ln1 = nn.LayerNorm(hidden_dim)
-        
-        self.fc2 = nn.Linear(hidden_dim, out_dim)
-        self.ln2 = nn.LayerNorm(out_dim)
-
-        self.fc3 = nn.Linear(out_dim, 1)
-        
-        self.Sigmoid = nn.Sigmoid()
-
-        nn.init.xavier_normal_(self.fc1.weight)
-        nn.init.xavier_normal_(self.fc2.weight)
-        nn.init.xavier_normal_(self.fc3.weight)
-        
-        self.block1 = nn.Sequential(self.fc1, self.ln1, nn.ReLU())
-        self.block2 = nn.Sequential(self.fc2, self.ln2) # embed
-        self.block3 = nn.Sequential(nn.ReLU(), self.fc3, self.Sigmoid)
-
-        
-    def forward(self, x):
-        x = self.block1(x)
-        x0 = self.block2(x)
-        x1 = self.block3(x0)
-        return x0, x1
 
 class Model5(nn.Module):
     def __init__(self, in_dim=5120,hidden_dim=2560, out_dim=1280):
